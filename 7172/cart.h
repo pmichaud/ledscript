7cc3edddb79ff8ef9bb65240fced99a6ace64726
pmichaud <pmichaud@pobox.com>
2016-05-12 23:53:56 -0500

    Update cart program to new formats.

--- 7172/cart.h
+++ 7172/cart.h
@@ -5,5 +5,29 @@
 
 #define LED_ORDER GRB
 #define LED_NUM 150
+#define CODE_NUM 256
+
+#define LED_CODE
+char code[CODE_NUM] = 
+    "+B/6C/6D/6E/6F/6GG/6H/6I/6B !> " // rainbow chase
+    "+:%ABCDEFGHI!%=600;"            // rainbow fades
+    "+:%ABCDEFGHI8:d30!%60 "         // rainbow dots
+    "+B "                            // red
+    "+AB9 !> "                       // red chase
+    "+G "                            // blue
+    "+AG9 !> "                       // blue chase
+    "+E "                            // green
+    "+AE9 !> "                       // green chase
+    "+C "                            // orange
+    "+D "                            // yellow
+    "+F "                            // aqua
+    "+H "                            // purple
+    "+I "                            // pink
+    "+A "                            // white
+    "+@ "                            // black
+    "+:%ACCC0!%500;"                 // random white-on-gold pixels
+    "+B6A6G6 !> "                    // red white blue chase
+    "+:%ABBBAGGG:q7,9!%60 "              // red white blue white random
+    ;
 
 #endif
